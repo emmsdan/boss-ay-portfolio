c83998040d56614ea7c6a4d64992849f5a360eed
Emmanuel Daniel <ecomje@gmail.com>
2020-04-13 14:09:47 +0200

    Added default font family
    Implement Footer area
    
    Implement Header
    
    created infocard module
    
    Fixed infocard and header overlap issues
    
    Test Implementation for InfoCard (DDT - Development Driven Test)
    
    added netlify ci to ci process. allows deploying to server
    
    added resume to app

---
+++ src/components/TheLoader.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="loader-container" v-if="!closeLoader">
+  <div class="loader">Loading...</div>
+  {{closeTime()}}
+</div>
+</template>
+<script>
+
+export default {
+  name: 'TheLoader',
+  props: {
+    timeout: {
+      type: String,
+      default: '10'
+    }
+  },
+  data () {
+    return {
+      closeLoader: false
+    }
+  },
+  computed: { },
+  methods: {
+    closeTime () {
+      const time = this.timeout * 1000
+      setTimeout(() => { this.closeLoader = true }, time)
+    }
+  }
+}
+</script>
---
+++ src/views/Resume.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="resume">
+    <Loader timeout="5"/>
+      <a :href="url" class="button is-primary">DOWNLOAD CV</a>
+    <PDFViewer
+      v-bind="{url}"
+      @document-rendered="onDocumentRendered"
+      @document-errored="onDocumentErrored"
+      />
+      <a :href="url" class="button is-primary">DOWNLOAD CV</a>
+  </div>
+</template>
+
+<script>
+import PDFViewer from '@/modules/PDFViewer/PDFViewer.vue'
+import Loader from '@/components/TheLoader.vue'
+export default {
+  name: 'Resume',
+
+  components: {
+    PDFViewer, Loader
+  },
+
+  data () {
+    return {
+      url: process.env.BASE_URL + 'emmauel-daniel-resume.pdf',
+      documentError: undefined,
+      enableUploader: false
+    }
+  },
+
+  methods: {
+    urlUpdated (url) {
+      this.documentError = undefined
+      this.url = url
+    },
+    onDocumentErrored (e) {
+      this.documentError = e.text
+    }
+  }
+
+}
+</script>
+
+<style>
+#resume {
+  max-width: 98%;
+  margin: auto;
+  margin-bottom: 20px;
+}
+#resume a {
+  margin: 20px 10px;
+}
+</style>
